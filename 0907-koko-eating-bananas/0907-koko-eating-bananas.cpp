17bb924fa38e7fdfd5c072529aaa1f8d68e6b2a8
Bhavishaya Khandelwal <99893459+BHAVISHAYA@users.noreply.github.com>
2024-07-31 00:08:58 +0530

    Time: 26 ms (87.25%), Space: 21.6 MB (38.33%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-
-    int check(vector<int>& piles, int h,int mid){
-        int ans=0;
-        for(int i=0;i<piles.size();i++){
-            ans+=(piles[i]/mid);
-            if(piles[i]%mid!=0){
-                ans++;
+    bool ableToEatBeforeGuardReturn(int mid, int h, vector<int> &piles) {
+        int ct = 0;
+        for(int i = 0 ; i < piles.size() ; i++) {
+            if(piles[i] <= mid) ct++;
+            else {
+                if(piles[i] % mid == 0) ct += piles[i] / mid;
+                else ct += ((piles[i] / mid) + 1);
             }
+            if(ct > h) return false;
         }
-        return ans<=h;
+        return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        sort(piles.begin(),piles.end());
-        int n=piles.size();
-        int low=1;
-        int high=piles[n-1];
-
-        while(low<high){
-            int mid=(low+high)/2;
-            if(check(piles,h,mid)){
-                high=mid;
+        // sort(piles.begin(), piles.end());
+        int start = 1;
+        int end = *max_element(piles.begin(), piles.end());
+        int ans = INT_MAX;
+        while(start <= end) {
+            int mid = ((start) + ((end - start) / 2));
+            if(ableToEatBeforeGuardReturn(mid, h, piles)) {
+                ans = min(ans, mid);
+                end = mid - 1;
             }
-            else{
-                low=mid+1;
+            else {
+                start = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
 };

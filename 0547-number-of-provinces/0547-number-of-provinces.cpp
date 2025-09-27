090bd7edfac8cc1580fca13b3cf88b19d3935ffc
Bhavishaya Khandelwal <99893459+BHAVISHAYA@users.noreply.github.com>
2025-09-27 07:52:05 +0530

    Time: 0 ms (100%), Space: 20.9 MB (30.48%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    void dfs(int startNode, vector<int> adj[], vector<int> &vis) {
+    void dfs(int startNode, vector<int> &vis, vector<int> adj[]) {
         vis[startNode] = 1;
         for(auto &value: adj[startNode]) {
-            if(vis[value] == 0) {
-                
-                // vis[value] = 1;
-                dfs(value, adj, vis);
+            if (vis[value] == 0) {
+                dfs(value, vis, adj);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int> adj[graph.size() + 1];
-        for(int i = 0 ; i < graph.size() ; i++) {
-            for(int j = 0 ; j < graph[i].size() ; j++) {
-                if(graph[i][j] == 1) {
-                    adj[i + 1].push_back(j + 1);
-                    adj[j + 1].push_back(i + 1);
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int provinces = 0;
+        vector<int> adj[isConnected.size()];
+        for(int i = 0 ; i < isConnected.size() ; i++) {
+            for(int j = 0 ; j < isConnected[i].size() ; j++) {
+                if(isConnected[i][j] == 1 && i != j) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
-        vector<int> vis(V + 1, 0);
-        int ans = 0;
-        for(int i = 1 ; i < (V + 1) ; i++) {
-            if(vis[i] == 0) {
-                dfs(i, adj, vis);
-                ans++;
+        vector<int> vis(isConnected.size(), 0);
+        for(int i = 0 ; i < isConnected.size() ; i++) {
+            if(!vis[i]) {
+                dfs(i, vis, adj);
+                provinces++;
             }
         }
-        return ans;
+        return provinces;
     }
 };
